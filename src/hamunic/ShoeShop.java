b80001ff2281b5aca1f7fc1ff207641f7f70e56f
Michael Fladischer <FladischerMichael@fladi.at>
2014-03-29 17:25:54 +0100

    Übung 2.
    Signed-off-by: Michael Fladischer <FladischerMichael@fladi.at>

--- src/hamunic/ShoeShop.java
+++ src/hamunic/ShoeShop.java
@@ -3,7 +3,11 @@
  */
 package hamunic;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Properties;
+import java.util.Stack;
 
 /**
  * @author FladischerMichael
@@ -20,12 +24,47 @@ public class ShoeShop {
 		try {
 			shoes = wh.getAllShoes(shoes);
 		} catch (Exception e) {
-			System.out.println(e.getMessage());
+			e.printStackTrace();
 			return;
 		}
-		System.out.println("Anzahl der Schuhe: " + shoes.size());
+
+		// Preisliste aus Warehouse laden.
+		Properties priceList;
+		try {
+			priceList = wh.getPriceList();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		// Arbeiter 1 stapelt die Schuhe übereinander.
+		Stack<Shoe> stackedShoes = new Stack<Shoe>();
 		for (Shoe shoe : shoes) {
-			System.out.println(" " + shoe);
+			// Jedes Paar Schuhe wird oben auf den Stapel gelegt.
+			stackedShoes.push(shoe);
+		}
+
+		// Arbeiter 2 nimmt die Schuhe von Stapel und ermittelt den Preis.
+		// Zuerst brauchen wir die LinkedList, in der wir dann alle Schuhe mit den jeweiligen PReisen ablegen wollen.
+		LinkedList<Shoe> pricedShoes = new LinkedList<Shoe>();
+		// Wir durchlaufen diese while-Schleife solange der stapel nicht leer ist.
+		while (!stackedShoes.empty()) {
+			// Das oberste Paar Schuhe vom Stapel nehmen.
+			Shoe shoe = stackedShoes.pop();
+			// Aus der Preisliste den Preis der Schuhe anhand ihres Herstellers und Namens holen.
+			String price = priceList.getProperty(shoe.getLabel() + "." + shoe.getName());
+			// Wass soll passieren, wenn die Schuhe nicht in der Preisliste stehen?
+			// Wir ignorieren sie einfach, fügen sie nicht in die LinkedList ein und machen mit dem nächsten Paar Schuhe weiter.
+			if (price != null) {
+				//Es kann passieren, dass in der Preisliste kein Geldbetrag hinterlegt ist, was dann?
+				try {
+					shoe.setPrice(Float.parseFloat(price));
+				} catch (NumberFormatException e) {
+					continue;
+				}
+				// Die Schuhe haben einen gültigen Preis, wir können sie an die LinkedList anfügen.
+				pricedShoes.add(shoe);
+			}
 		}
 
 	}
